2cfd98e5322a32aa7c87cc1815d288915c5fcb83
teddy GAUTHIER <capitaine_ted@hotmail.fr>
2019-12-19 10:53:22 +0100

    docs : maj fichier config

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
-//rien pour l'instant
-console.log('test')
+const express:any = require('express')
+const app:any = express()
+
+const port:number = 8080
+const domain:string = 'http://localhost:' + port + '/'
+
+app.set('view engine', 'pug')
+
+app.use('/assets', express.static('public'))
+app.use(express.urlencoded({extended : true}))
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.listen(port, () => console.log('Ecoute le port', port, '...'))
